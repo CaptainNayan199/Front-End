1eec2429ffb8d0db2874340d5810196344300109
Captainnayan199 <nayanpathak233199@gmail.com>
2025-07-25 16:18:29 +0545

    typeof.js file added

---
+++ JS/Numbers & String/typeof.js
@@ -0,0 +1,8 @@
+// typeof operator is used to get the type of any variable.
+name = 'Nayan'
+console.log(typeof (name)) //string
+
+console.log(typeof 4) //number
+
+
+console.log(typeof true) //boolean
