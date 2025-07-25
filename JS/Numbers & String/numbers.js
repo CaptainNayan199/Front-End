29eb8ec8f18a539e8e71ea5f4e5b5a746ca3260a
Captainnayan199 <nayanpathak233199@gmail.com>
2025-07-25 16:16:32 +0545

    Strings.js added

--- JS/Numbers & String/numbers.js
+++ JS/Numbers & String/numbers.js
@@ -1,3 +1,6 @@
 // There are two types of number in JS - Integer and Floating point.
 // Floating points are the decimal values.
 // Integers are ngative, 0, and positive numbers.
+
+num1 = 10 //integers
+num2 = 5.6 //floating numbers.
---
+++ JS/Numbers & String/strings.js
@@ -0,0 +1,13 @@
+// Strings are alphabetical values, characters wrapped inside single/double quotes or backticks.
+name = "Nayan"
+address = 'Earth'
+country = `Idk
+why are you asking my country name.
+`
+
+// Strings holds textual data, anything from a single character to paragraph.
+
+// Concatenation - Combining two or more than two strings.
+
+console.log(name + " is a good person.")
+console.log('4'+'5') //45 will be the output as we are adding string 4 and string 5 hence js concatenates it.
