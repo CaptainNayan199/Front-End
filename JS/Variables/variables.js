164741fa7539f07bd4393226a017a7fde3abf33c
Captainnayan199 <nayanpathak233199@gmail.com>
2025-07-25 21:52:37 +0545

    variables.js file updated

---
+++ JS/Variables/variables.js
@@ -0,0 +1,24 @@
+// What are variables ? -> Variables are the container where something(values in our case) is stored. Variables are relevant and appropriate containe to store variables.
+
+var name = 'Sita'
+var age = 20
+var isMarried = false
+
+// So here in the above few lines name, age and isMarried are variables/containers.
+// The best way to define variable is using let(most common). Var is global scope and let is block scope.
+
+let myName = "Nayan"
+console.log(myName)
+
+// Variables cannot be named as reserved keywords, cannot be started with numbers, no other special character other than $ and _.
+
+// Updating values
+
+let myAge = 20
+
+myAge = myAge + 1 //21
+myAge -= 1 // this is equal to -> myAge = myAge - 1 = 20
+myAge += 1 //21
+myAge /= 1 //21
+myAge *= 2 //42
+myAge++ // 43
