9ccb76e210c907dc187645dda58455dddaf2983b
Captainnayan199 <nayanpathak233199@gmail.com>
2025-07-25 16:00:47 +0545

    New file numbers.js added

---
+++ JS/Numbers & String/arithmetic.js
@@ -0,0 +1,6 @@
+// There are many operatod are like +, -, *, /, %(modulus - for obtaining remainder), etc.
+console.log(4 + 2)
+console.log(4 - 2)
+console.log(4 * 2)
+console.log(4 / 2)
+console.log(4 % 2)
---
+++ JS/Numbers & String/numbers.js
@@ -0,0 +1,3 @@
+// There are two types of number in JS - Integer and Floating point.
+// Floating points are the decimal values.
+// Integers are ngative, 0, and positive numbers.
