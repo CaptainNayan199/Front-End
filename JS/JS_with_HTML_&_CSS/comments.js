9800e927382ef8bcec51fc4c032d6137aec92af7
Captainnayan199 <nayanpathak233199@gmail.com>
2025-07-25 17:31:30 +0545

    new file comment.js added and wrapped up this sectin

---
+++ JS/JS_with_HTML_&_CSS/comments.js
@@ -0,0 +1,11 @@
+// This is a comment - Comments are the non-executable file. Comments are for human, codes are for machines. This is a single line comment.
+
+/* 
+This
+is 
+a 
+multi
+line
+comment.
+
+*/
