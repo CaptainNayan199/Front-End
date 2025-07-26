dfabe2ab76ddb69048cbdbacd6d87c29bd4731dd
Captainnayan199 <nayanpathak233199@gmail.com>
2025-07-26 21:01:05 +0545

    new file naming_conventions.js added

---
+++ JS/Variables/naming_convention.js
@@ -0,0 +1,7 @@
+// Naming conventions is the way or method how we define our variable names.
+// There are multiple types of conventions : camel case, snake case etc
+
+// camel case - Start with lowercase letter and capitalize the first letter of each subsequent word.
+let camelCaseExample
+// snake case - start with lower case word and add underscore(_) and give another word
+let snake_case_example
