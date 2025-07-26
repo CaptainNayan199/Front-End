91936d4749ffc873d41134495aaaf57aa284113c
Captainnayan199 <nayanpathak233199@gmail.com>
2025-07-26 21:07:20 +0545

    create_variable.js file added

---
+++ JS/Variables/create_variables.js
@@ -0,0 +1,10 @@
+// In js there are majorly 3 ways to define and create a variable
+
+//  const - assigning constant values. Thos values that are guranteed to be not updated or changed in future. Variables defined with const cant be modified afterward in any instance.
+const pi = 3.1415
+
+// var - traditional method of defining a variable. Not used nowadays. This type of variable can be accessed anywhere from anywhere.
+var name = 'Nayan'
+
+// let - Only can be accessed in that specific block where it is defined or created.
+let age = 20
