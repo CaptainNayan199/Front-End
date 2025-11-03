9f5362a63e23509589f86274d775388acfc81963
Captainnayan199 <nayanpathak233199@gmail.com>
2025-11-03 19:56:42 +0545

    Shorcuts added

--- JS/Objects/shortcuts.js
+++ JS/Objects/shortcuts.js
@@ -8,12 +8,25 @@ let obj = {
 }
 
 
-// De structuring : extracting properties from objects
-// In place of :
+// *************De structuring : extracting properties from objects
 
+// In place of :
 let naaam = obj.name
+console.log(naaam);
 
 // we can do this
-
 let { name } = obj
 // but the variable name should be same as that in object. we can alter the varibale as that will not allow obj to find for that value.
+console.log(name);
+
+let { age, address, isMarried } = obj
+console.log(age, address, isMarried)
+
+// ***********Short Hand property : We can just write the key and value of that key will be the same key. But key should be from outside.
+
+let price = prompt('Please enter the price : ')
+let obj2 = {
+  name: 'Gold',
+  symbol: 'Au',
+  price // so here, theres only price, that means it is actually like this (price: price), but we can only write price in this case. This is shorthand property of object.
+}
