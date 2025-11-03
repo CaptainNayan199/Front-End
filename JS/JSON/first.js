38723584c6b490e37d9e99b134f9adb57fc8e50c
Captainnayan199 <nayanpathak233199@gmail.com>
2025-11-03 20:18:19 +0545

    New JSON added

---
+++ JS/JSON/first.js
@@ -0,0 +1,27 @@
+// JSON stands for JavaScript Object Notation. It is not the same as JS object, but similar.
+// JSON is highyl common in network calls, api calls, and data storage.
+// There are two popular methods :
+// 1. JSON.stringfy() -> converts JSON objects to JS object.
+// 2. JSON.parse() -> converts JS objects to JSON objects.
+// JSON is data format, where as JS object is a data structure.
+
+// This below is JS object.
+let jsObj = {
+  firstName: 'Nayan',
+  lastName: 'Mars'
+}
+
+// This below is JSON object. Key value all should be enclosed in a double quotation.
+let jsonObj = {
+  "name": "Nayan",
+  "thar": "Mars"
+}
+
+
+let newStr = JSON.stringify(jsObj) // converting to normal object(string)
+console.log(newStr);
+
+
+let newObj = JSON.parse(newStr) // converting string back to object.
+console.log(newObj);
+
