218e8fb0dbeb5f17edad5abe941ca07d3bb019a1
Captainnayan199 <nayanpathak233199@gmail.com>
2025-11-03 15:35:35 +0545

    auto boxing added

---
+++ JS/Objects/auto-boxing.js
@@ -0,0 +1,8 @@
+// Auto boxing - automatic conversion of primitives to an object.
+
+let namee = 'Nayan'
+console.log(namee.toUpperCase());
+console.log(namee.length);
+
+// So here, string namee is a primitive type in js, but we have converted it to an object and then applied all different methods available, this is what auto-boxing means.
+
